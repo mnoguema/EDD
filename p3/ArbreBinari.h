9473cb11da7c668586e1cde1b47b44eb5aa96199
mnoguema <mnoguera00@gmail.com>
2023-05-15 18:46:45 +0200

    mac

--- p3/ArbreBinari.h
+++ p3/ArbreBinari.h
@@ -5,29 +5,34 @@
 
 #include "NodeBinari.h"
 
+using namespace std;
+
 template <class Clau, class Valor>
-class ArbreBinari {
-    public:
-        ArbreBinari();
-        ArbreBinari(const ArbreBinari<Clau, Valor>& orig);
-        virtual ~ArbreBinari();
+class ArbreBinari
+{
+public:
+    ArbreBinari();
+    ArbreBinari(const ArbreBinari<Clau, Valor> &orig);
+    virtual ~ArbreBinari();
 
-        bool isEmpty() const;
-        int height() const;
+    bool isEmpty() const;
+    int height() const;
 
-        NodeBinari<Clau,Valor>* insert(const Clau& clau, const Valor& value);
-        const Valor& valueOf(const Clau& clau) const;
-        void imprimirPreordre(const NodeBinari<Clau,Valor>* n = nullptr) const;
-        void imprimirInordre(const NodeBinari<Clau,Valor>* n = nullptr) const;
-        void imprimirPostordre(const NodeBinari<Clau,Valor>* n = nullptr) const;
-        // vector<NodeBinari<Clau,Valor>*> obteValorsRang(int k1, int k2) const;
-        // void imprimirCami(Clau key) const;
-        void eliminaMinim();
-    protected:
-        NodeBinari<Clau,Valor>* root;
-        NodeBinari<Clau,Valor>* search(const Clau& k) const;
-    private:
-        int _size;
- /* Mètodes auxiliars definiu aquí els que necessiteu */
+    NodeBinari<Clau, Valor> *insert(const Clau &clau, const Valor &value);
+    const Valor &valueOf(const Clau &clau) const;
+    void imprimirPreordre(const NodeBinari<Clau, Valor> *n = nullptr) const;
+    void imprimirInordre(const NodeBinari<Clau, Valor> *n = nullptr) const;
+    void imprimirPostordre(const NodeBinari<Clau, Valor> *n = nullptr) const;
+    // vector<NodeBinari<Clau,Valor>*> obteValorsRang(int k1, int k2) const;
+    // void imprimirCami(Clau key) const;
+    void eliminaMinim();
+
+protected:
+    NodeBinari<Clau, Valor> *root;
+    NodeBinari<Clau, Valor> *search(const Clau &k) const;
+
+private:
+    int _size;
+    /* Mètodes auxiliars definiu aquí els que necessiteu */
 };
-#endif //ARBREBINARI_H
+#endif // ARBREBINARI_H
